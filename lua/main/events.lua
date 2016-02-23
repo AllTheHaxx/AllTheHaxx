6298c80018d4419e0f87a2445182adec070898d1
Alex <qh6ma@web.de>
2016-02-23 20:40:55 +0100

    Eventtables are now generated dynamically if a first function into this EventName is added; merged init.lua & events.lua

--- lua/main/events.lua
+++ lua/main/events.lua
@@ -1,55 +1,70 @@
+Events = {}
+
+-- YOU SHOULD NOT TOUCH THIS FUNCTION IF YOU DON'T KNOW WHAT YOU ARE DOING!
+function RegisterEvent(EventName, Func)
+	if Events[EventName] == nil then  --create new table if its empty
+		Events[EventName] = {}
+	end
+	
+	Events[EventName][Func] = Func
+end
+
 function OnChat(ID, Team, Message)
-	for script, event in pairs(Events["OnChat"]) do
-		if event ~= nil then
-			event(ID, Team, Message)
+	if Events["OnChat"] ~= nil then
+		for script, event in pairs(Events["OnChat"]) do
+			if event ~= nil then
+				event(ID, Team, Message)
+			end
 		end
 	end
 end
 
 function OnEnterGame()
-	for script, event in pairs(Events["OnEnterGame"]) do
-		if event ~= nil then
-			event()
+	if Events["OnEnterGame"] ~= nil then
+		for script, event in pairs(Events["OnEnterGame"]) do
+				event()
 		end
 	end
 end
 
 function OnKill(Killer, Victim, Weapon)
-	for script, event in pairs(Events["OnKill"]) do
-		if event ~= nil then
-			event(Killer, Victim, Weapon)
+	if Events["OnKill"] ~= nil then
+		for script, event in pairs(Events["OnKill"]) do
+				event(Killer, Victim, Weapon)
 		end
 	end
 end
 
 function OnMessageIRC(From, User, Message)
-	for script, event in pairs(Events["OnMessageIRC"]) do
-		if event ~= nil then
-			event(From, User, Message)
+	if Events["OnMessageIRC"] ~= nil then
+		for script, event in pairs(Events["OnMessageIRC"]) do
+				event(From, User, Message)
 		end
 	end
 end
 
 function OnRenderBackground()
-	for script, event in pairs(Events["OnRenderBackground"]) do
-		if event ~= nil then
-			event()
+	if Events["OnRenderBackGround"] ~= nil then
+		for script, event in pairs(Events["OnRenderBackground"]) do
+			if event ~= nil then
+				event()
+			end
 		end
 	end
 end
 
 function OnRenderScoreboard()
-	for script, event in pairs(Events["OnRenderScoreboard"]) do
-		if event ~= nil then
-			event()
+	if Events["OnRenderScoreboard"] ~= nil then
+		for script, event in pairs(Events["OnRenderScoreboard"]) do
+				event()
 		end
 	end
 end
 
 function OnStateChange(NewState, OldState)
-	for script, event in pairs(Events["OnStateChange"]) do
-		if event ~= nil then
-			event(NewState, OldState)
+	if Events["OnStateChange"] ~= nil then
+		for script, event in pairs(Events["OnStateChange"]) do
+				event(NewState, OldState)
 		end
 	end
 end
