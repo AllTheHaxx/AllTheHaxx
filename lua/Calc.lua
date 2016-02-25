d38078445f7e32d335dcec8940e8ca3196df0234
xushTW <g8730443@trbvm.com>
2016-02-25 21:42:57 +0100

    avoid conflicts with translator

--- lua/Calc.lua
+++ lua/Calc.lua
@@ -1,6 +1,6 @@
 local function Calc(ClientID, Team, Message)
-	if(string.sub(Message, 1, string.len("!calc")) == "!calc") then
-		x = string.sub(Message, string.len("!calc"), string.len(Message))
+	if(string.sub(Message, 1, string.len(".calc")) == ".calc") then
+		x = string.sub(Message, string.len(".calc"), string.len(Message))
 		func = assert(load("return " .. x))
 		y = func()
 		_game.chat.Send(0, "Result: "..y)
