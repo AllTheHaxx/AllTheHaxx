081b5263a770b5cf1760baaf91867bd320a75d96
xushTW <g8730443@trbvm.com>
2016-02-25 21:41:19 +0100

    Calc.lua script

---
+++ lua/Calc.lua
@@ -0,0 +1,11 @@
+local function Calc(ClientID, Team, Message)
+	if(string.sub(Message, 1, string.len("!calc")) == "!calc") then
+		x = string.sub(Message, string.len("!calc"), string.len(Message))
+		func = assert(load("return " .. x))
+		y = func()
+		_game.chat.Send(0, "Result: "..y)
+		print("Result: "..y)
+	end
+end
+
+RegisterEvent("OnChat", Calc)
