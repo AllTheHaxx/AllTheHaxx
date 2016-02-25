2b91b644350de8bf6edeca2257dceb35657f4d68
xushTW <g8730443@trbvm.com>
2016-02-25 21:11:18 +0100

    tweaked last commit a bit

--- lua/GreetBot.lua
+++ lua/GreetBot.lua
@@ -4,8 +4,7 @@ local function GreetBot(ClientID, Team, Message)
 		string.find(string.lower(Message), "hello") or
 		string.find(string.lower(Message), "hey") or
 		string.find(string.lower(Message), "yo") or
-		string.find(string.lower(Message), "what's up") or
-		string.find(string.lower(Message), "whats up") or
+		string.find(string.lower(Message), "s up") or -- to detect whats up, what's up
 		string.find(string.lower(Message), "howdie") or
 		string.find(string.lower(Message), "sup")) then
 			_game.chat.Send(0, "Hey ".._client.GetPlayerName(ClientID))
