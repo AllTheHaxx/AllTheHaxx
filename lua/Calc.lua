cda52c0e6a0380728b023cf81a9a58e0a65a25ca
Henningstone <Henningstone@users.noreply.github.com>
2016-03-05 13:27:04 +0100

    Fixed Calc.lua

--- lua/Calc.lua
+++ lua/Calc.lua
@@ -1,9 +1,9 @@
-local function Calc(ClientID, Team, Message)
+function Calc(ClientID, Team, Message)
 	if(string.sub(Message, 1, string.len(".calc")) == ".calc") then
 		x = string.sub(Message, string.len(".calc")+2, string.len(Message))
 		func = assert(load("return "..x))
 		y = func()
-		_game.chat.Send(0, _client.GetPlayerName(ClientID) .. ": " .. x.." = "..y)
+		Game.Chat:Say(0, Game.Players(ClientID).Name .. ": " .. x.." = "..y)
 		print("Result: "..x.." = "..y)
 	end
 end
