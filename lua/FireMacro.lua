b193b33d47bcde24d538c497d75065a6af395ac0
Henningstone <Henningstone@users.noreply.github.com>
2016-03-05 02:30:51 +0100

    Fixed FireMacro.lua

--- lua/FireMacro.lua
+++ lua/FireMacro.lua
@@ -1,19 +1,19 @@
 -- Probably remove this before release, devs wouldn't approve
 RapidFireActive = false
 
-local function RapidFire()
+function RapidFire()
 	if(RapidFireActive == true) then
-		_game.controls.SetInput("Fire", _game.controls.GetInput("Fire")+1)
+		Game.Input.Fire = Game.Input.Fire + 1
 	end
 end
 
-local function RapidFireOn(Key)
+function RapidFireOn(Key)
 	if(Key == "mouse1") then
 		RapidFireActive = true
 	end
 end
 
-local function RapidFireOff(Key)
+function RapidFireOff(Key)
 	if(Key == "mouse1") then
 		RapidFireActive = false
 	end
