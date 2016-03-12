ed09760e7d65bda2a201cded7458af5d4593295b
xushTW <g8730443@trbvm.com>
2016-03-12 09:19:55 +0100

    cleaned up translator a bit

--- src/game/client/components/translator.cpp
+++ src/game/client/components/translator.cpp
@@ -28,6 +28,7 @@ bool CTranslator::Init()
 
 CTranslator::~CTranslator()
 {
+	// clean up
 	if(m_pHandle)
 		curl_easy_cleanup(m_pHandle);
 	curl_global_cleanup();
@@ -48,30 +49,7 @@ void CTranslator::TranslationWorker(void *pUser)
 			char aPost[2048*8];
 			char aResponse[2048*8];
 			char aTranslated[1024*8];
-			// aLang[64];
 			std::string response;
-
-			/*if(str_comp(Entry.m_SrcLang, "detect") == 0)
-			{
-				curl_easy_setopt(pTrans->m_pHandle, CURLOPT_URL, "https://api.idolondemand.com/1/api/sync/identifylanguage/v1");
-				str_format(aPost, sizeof(aPost), "apikey=7b45313b-e5f1-4a8b-9d5f-0f0f5b72e349&text=%s", Entry.m_Text);
-			    curl_easy_setopt(pTrans->m_pHandle, CURLOPT_POSTFIELDS, aPost);
-			 	
-				curl_easy_setopt(pTrans->m_pHandle, CURLOPT_WRITEFUNCTION, &CTranslator::write_to_string);
-				curl_easy_setopt(pTrans->m_pHandle, CURLOPT_WRITEDATA, &response);
-				curl_easy_perform(pTrans->m_pHandle);
-
-				str_copy(aResponse, response.c_str(), sizeof(aResponse));
-
-				json_value *pVal = json_parse(aResponse);
-				str_copy(aLang, json_string_get(json_object_get(pVal, "language")), sizeof(aLang));
-				dbg_msg("trans", "detected language: '%s'", aLang);
-
-				mem_zero(aPost, sizeof(aPost));
-				mem_zero(aResponse, sizeof(aResponse));
-				mem_zero(aTranslated, sizeof(aTranslated));
-				mem_zero(&response, sizeof(&response));
-			}*/
 
 			curl_easy_setopt(pTrans->m_pHandle, CURLOPT_URL, "http://api.mymemory.translated.net/get");
 			str_format(aPost, sizeof(aPost), "q=%s&langpair=%s|%s&de=associatingblog@gmail.com", Entry.m_Text, Entry.m_SrcLang, Entry.m_DstLang);
@@ -83,6 +61,7 @@ void CTranslator::TranslationWorker(void *pUser)
 
 			str_copy(aResponse, response.c_str(), sizeof(aResponse));
 
+			// parse response
 			json_value *pValue = json_parse(aResponse);
 			str_copy(aTranslated, json_string_get(json_object_get(json_object_get(pValue,"responseData"),"translatedText")), sizeof(aTranslated));
 			if(str_comp_nocase(Entry.m_Text, aTranslated) != 0)
@@ -96,7 +75,7 @@ void CTranslator::TranslationWorker(void *pUser)
 			else
 				dbg_msg("trans", "translating '%s' from '%s' to '%s' failed", Entry.m_Text, Entry.m_SrcLang, Entry.m_DstLang);
 
-			// done, remove the element from out queue
+			// done, remove the element from our queue
 			pTrans->Queue.erase(pTrans->Queue.begin());
 		}
 	}
@@ -104,6 +83,7 @@ void CTranslator::TranslationWorker(void *pUser)
 
 void CTranslator::RequestTranslation(const char *pSrcLang, const char *pDstLang, const char *pText, bool In)
 {
+	// prepare the entry
 	CTransEntry Entry;
 	str_copy(Entry.m_Text, pText, sizeof(Entry.m_Text));
 	str_copy(Entry.m_SrcLang, pSrcLang, sizeof(Entry.m_SrcLang));
