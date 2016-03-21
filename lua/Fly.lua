28d0bef8d78376dd3b5ee698ab2e9d86ee96298f
xushTW <g8730443@trbvm.com>
2016-03-21 15:48:08 +0100

    removed some scripts that could be considered a 'hack'

--- lua/Fly.lua
+++
@@ -1,8 +0,0 @@
-g_ScriptTitle = "Fly"
-g_ScriptInfo = "Life's a bitch until ya die, that's why we get high. | by the AllTheHaxx-Team"
-
-function OnTick()
-	Game.Input.Hook = ((Game.LocalTee.Vel.y > 0) and 1 or 0)
-end
-
-RegisterEvent("OnTick", "OnTick")
