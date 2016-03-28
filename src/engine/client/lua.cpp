53f22806ea2ab787f4a8de2c71f665df1eae7665
Henningstone <Henningstone@users.noreply.github.com>
2016-03-28 19:01:46 +0200

    Replace 4-space indentation by tab

--- src/engine/client/lua.cpp
+++ src/engine/client/lua.cpp
@@ -121,43 +121,43 @@ int CLua::LoadFolderCallback(const char *pName, int IsDir, int DirType, void *pU
 
 int CLua::Panic(lua_State *L)
 {
-    dbg_break();
-    return 0;
+	dbg_break();
+	return 0;
 }
 
 int CLua::ErrorFunc(lua_State *L)
 {
 	dbg_msg("Lua", "Lua Script Error! :");
-    //lua_getglobal(L, "pLUA");
-    //CLua *pSelf = (CLua *)lua_touserdata(L, -1);
-    //lua_pop(L, 1);
+	//lua_getglobal(L, "pLUA");
+	//CLua *pSelf = (CLua *)lua_touserdata(L, -1);
+	//lua_pop(L, 1);
 
-    //int depth = 0;
-    //int frameskip = 1;
-    //lua_Debug frame;
+	//int depth = 0;
+	//int frameskip = 1;
+	//lua_Debug frame;
 
-    if (lua_tostring(L, -1) == 0)
+	if (lua_tostring(L, -1) == 0)
 	{
 		//dbg_msg("Lua", "PANOS");
-        return 0;
+		return 0;
 	}
 	
-    //dbg_msg("Lua", pSelf->m_aFilename);
-    dbg_msg("Lua", lua_tostring(L, -1));
-    /*dbg_msg("Lua", "Backtrace:");
+	//dbg_msg("Lua", pSelf->m_aFilename);
+	dbg_msg("Lua", lua_tostring(L, -1));
+	/*dbg_msg("Lua", "Backtrace:");
 
-    while(lua_getstack(L, depth, &frame) == 1)
-    {
-        depth++;
-        lua_getinfo(L, "nlSf", &frame);
-        // check for functions that just report errors. these frames just confuses more then they help
-        if(frameskip && str_comp(frame.short_src, "[C]") == 0 && frame.currentline == -1)
-            continue;
-        frameskip = 0;
-        // print stack frame
-        dbg_msg("Lua", "%s(%d): %s %s", frame.short_src, frame.currentline, frame.name, frame.namewhat);
-    }*/
-    lua_pop(L, 1); // remove error message
-    lua_gc(L, LUA_GCCOLLECT, 0);
-    return 0;
+	while(lua_getstack(L, depth, &frame) == 1)
+	{
+		depth++;
+		lua_getinfo(L, "nlSf", &frame);
+		// check for functions that just report errors. these frames just confuses more then they help
+		if(frameskip && str_comp(frame.short_src, "[C]") == 0 && frame.currentline == -1)
+			continue;
+		frameskip = 0;
+		// print stack frame
+		dbg_msg("Lua", "%s(%d): %s %s", frame.short_src, frame.currentline, frame.name, frame.namewhat);
+	}*/
+	lua_pop(L, 1); // remove error message
+	lua_gc(L, LUA_GCCOLLECT, 0);
+	return 0;
 }
