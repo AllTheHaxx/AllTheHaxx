70ee642f5164a6c9303f8188c4b22725b25226f0
Henningstone <Henningstone@users.noreply.github.com>
2016-04-01 01:21:46 +0200

    Maybe fixed the mysterious hung

--- src/game/client/components/hud.cpp
+++ src/game/client/components/hud.cpp
@@ -392,6 +392,7 @@ void CHud::RenderNotifications()
 		return;
 
 	const float NOTIFICATION_LIFETIME = 15.0f; // in seconds
+	const float TEXT_SIZE = 6.0f;
 	const float Y_BOTTOM = m_Height/1.7f;
 
 	// render background
@@ -400,10 +401,10 @@ void CHud::RenderNotifications()
 		// check for the required number of lines
 		int NumLines = 0;
 		for(int i = 0; i < m_Notifications.size(); i++)
-			NumLines += TextRender()->TextLineCount(0, 6.4f, m_Notifications[i].m_aMsg, m_Width/4.3f);
+			NumLines += TextRender()->TextLineCount(0, TEXT_SIZE, m_Notifications[i].m_aMsg, m_Width/4.3f);
 
-		float ybottom = Y_BOTTOM + 6.3f/2;
-		float ytop = ybottom - NumLines*6.3f - 6.3f/2;
+		float ybottom = Y_BOTTOM + TEXT_SIZE/2;
+		float ytop = ybottom - NumLines*TEXT_SIZE - TEXT_SIZE/2;
 		float height = ybottom-ytop;
 		CUIRect r;
 		r.x = m_Width-m_Width/4.3f-2.5f; r.y = ytop; r.w = m_Width/4.3f+2.5f; r.h = height;
@@ -415,7 +416,7 @@ void CHud::RenderNotifications()
 	for(int i = 0; i < m_Notifications.size(); i++)
 	{
 		CNotification *n = &m_Notifications[i];
-		yOffset += (TextRender()->TextLineCount(0, 6.4f, n->m_aMsg, m_Width/4.3f)-1)*6.4f;
+		yOffset += (TextRender()->TextLineCount(0, TEXT_SIZE, n->m_aMsg, m_Width/4.3f)-1)*TEXT_SIZE;
 
 		float FadeVal = (n->m_SpawnTime + NOTIFICATION_LIFETIME-Client()->LocalTime()) / NOTIFICATION_LIFETIME;
 
@@ -439,7 +440,7 @@ void CHud::RenderNotifications()
 			n->m_xOffset = 0.0f;
 			TextRender()->TextColor(n->m_Color.r, n->m_Color.g, n->m_Color.b, min(FadeVal, n->m_Color.a));
 		}
-		TextRender()->Text(0, m_Width-m_Width/4.3f+2.5f-(n->m_xOffset-=n->m_xOffset/15), Y_BOTTOM-(yOffset+=6.3f), 6.4f, n->m_aMsg, m_Width/4.3f);
+		TextRender()->Text(0, m_Width-m_Width/4.3f+2.5f-(n->m_xOffset-=n->m_xOffset/15), Y_BOTTOM-(yOffset+=TEXT_SIZE), TEXT_SIZE, n->m_aMsg, m_Width/4.3f);
 		//if(TextRender()->TextLineCount(0, 6.4f, n->m_aMsg, m_Width/4.3f) > 1)
 		//yOffset += (TextRender()->TextLineCount(0, 6.4f, n->m_aMsg, m_Width/4.3f)-1)*6.3f;
 	}
