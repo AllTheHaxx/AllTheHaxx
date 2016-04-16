7edf7d16c2500d0e500900e840a291812bc0d476
Henningstone <Henningstone@users.noreply.github.com>
2016-04-17 00:19:34 +0200

    Fixed grey background while connecting (uses default one), closes #118

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -1372,7 +1372,9 @@ int CMenus::Render()
 		ms_ColorTabbarActive = ms_ColorTabbarActiveOutgame;
 	}
 
-	if(!Client()->MapLoaded())// && Client()->State() != IClient::STATE_ONLINE)
+	if(	!Client()->MapLoaded() ||
+			Client()->State() == IClient::STATE_CONNECTING ||
+			Client()->State() == IClient::STATE_LOADING)// && Client()->State() != IClient::STATE_ONLINE)
 		RenderBackground();
 
 	CUIRect TabBar;
