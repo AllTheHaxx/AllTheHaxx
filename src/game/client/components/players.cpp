5c86f81a6e596532c079ec5d9ec70c7a2835c338
Henningstone <Henningstone@users.noreply.github.com>
2016-04-16 20:04:21 +0200

    Fix ATH sign overlapping name, closes #124

--- src/game/client/components/players.cpp
+++ src/game/client/components/players.cpp
@@ -939,13 +939,18 @@ void CPlayers::RenderPlayer(
 					TextRender()->TextColor(0.7f, 0.7f, 1.0f, a);
 			}
 
-			TextRender()->Text(0, Position.x-tw/2.0f, Position.y-FontSize-38.0f-FontSize, FontSize, aName, -1);
-
-			if(g_Config.m_ClNameplatesClan)
+			// name + clan / only name
+			if(m_pClient->m_aClients[pPlayerInfo->m_ClientID].m_aClan[0] != '\0' && g_Config.m_ClNameplatesClan)
 			{
+				TextRender()->Text(0, Position.x - tw / 2.0f, Position.y - FontSize - 38.0f - FontSize, FontSize, aName, -1); // name above clan
+
 				const char *pClan = m_pClient->m_aClients[pPlayerInfo->m_ClientID].m_aClan;
 				float tw_clan = TextRender()->TextWidth(0, FontSizeClan, pClan, -1);
 				TextRender()->Text(0, Position.x-tw_clan/2.0f, Position.y - FontSizeClan - 38.0f, FontSizeClan, pClan, -1);
+			}
+			else
+			{
+				TextRender()->Text(0, Position.x - tw / 2.0f, Position.y - FontSize - 38.0f, FontSize, aName, -1); // only name
 			}
 
 			CServerInfo ServerInfo;
