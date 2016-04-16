36848edb0f12caa0f71cbfdf87d0b2fca76e67be
Henningstone <Henningstone@users.noreply.github.com>
2016-04-16 16:49:38 +0200

    Support for multiple SQL databases + cleanups

--- src/engine/client/db_sqlite3.h
+++ src/engine/client/db_sqlite3.h
@@ -1,5 +1,5 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
-/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+/* If you are missing that file, acquire a complete release at teeworlds.com.				*/
 
 #ifndef ENGINE_CLIENT_SQLITE3_H
 #define ENGINE_CLIENT_SQLITE3_H
@@ -12,47 +12,49 @@
 
 class CQuery
 {
-    friend class CSql;
+	friend class CSql;
+
 private:
-    std::string m_Query;
-    sqlite3_stmt *m_pStatement;
-    virtual void OnData();
+	std::string m_Query;
+	sqlite3_stmt *m_pStatement;
+	virtual void OnData();
+
 public:
-    virtual ~CQuery() {}
+	virtual ~CQuery() {}
 
-    bool Next();
-    int GetColumnCount() { return sqlite3_column_count(m_pStatement); }
-    const char *GetName(int i) { return sqlite3_column_name(m_pStatement, i); }
-    int GetType(int i) { return sqlite3_column_type(m_pStatement, i); }
+	bool Next();
+	int GetColumnCount() { return sqlite3_column_count(m_pStatement); }
+	const char *GetName(int i) { return sqlite3_column_name(m_pStatement, i); }
+	int GetType(int i) { return sqlite3_column_type(m_pStatement, i); }
 
-    int GetID(const char *pName);
-    int GetInt(int i) { return sqlite3_column_int(m_pStatement, i); }
-    float GetFloat(int i) { return sqlite3_column_double(m_pStatement, i); }
-    const char *GetText(int i) { return (const char *)sqlite3_column_text(m_pStatement, i); }
-    const void *GetBlob(int i) { return sqlite3_column_blob(m_pStatement, i); }
-    int GetSize(int i) { return sqlite3_column_bytes(m_pStatement, i); }
+	int GetID(const char *pName);
+	int GetInt(int i) { return sqlite3_column_int(m_pStatement, i); }
+	float GetFloat(int i) { return sqlite3_column_double(m_pStatement, i); }
+	const char *GetText(int i) { return (const char *)sqlite3_column_text(m_pStatement, i); }
+	const void *GetBlob(int i) { return sqlite3_column_blob(m_pStatement, i); }
+	int GetSize(int i) { return sqlite3_column_bytes(m_pStatement, i); }
 
-    class CSql *m_pDatabase;
-    void Query(class CSql *pDatabase, char *pQuery);
+	class CSql *m_pDatabase;
+	void Query(class CSql *pDatabase, char *pQuery);
 };
 
 class CSql
 {
 private:
-    void WorkerThread();
-    static void InitWorker(void *pSelf);
-    LOCK m_Lock;
+	void WorkerThread();
+	static void InitWorker(void *pSelf);
+	LOCK m_Lock;
 
-    std::queue<CQuery *>m_lpQueries;
+	std::queue<CQuery *>m_lpQueries;
 
-    bool m_Running;
+	bool m_Running;
 
-    sqlite3 *m_pDB;
+	sqlite3 *m_pDB;
 
 public:
-    CSql();
-    ~CSql();
-    CQuery *Query(CQuery *pQuery, std::string QueryString);
+	CSql(const char *pFilename = "ath_data");
+	~CSql();
+	CQuery *Query(CQuery *pQuery, std::string QueryString);
 
 };
 
