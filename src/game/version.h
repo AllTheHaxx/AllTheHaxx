065cde7bca63850f4518d2227d97e644f01666e6
Henningstone <Henningstone@users.noreply.github.com>
2016-04-30 13:31:03 +0200

    check if GIT_SHORTREV_HASH is available

--- src/game/version.h
+++ src/game/version.h
@@ -5,7 +5,13 @@
 #include "generated/nethash.cpp"
 #define GAME_VERSION "0.6.3"
 #define BUILD_DATE __DATE__ ", " __TIME__
-#define ALLTHEHAXX_VERSION "beta-13.1 (v0.9-3.1) #" GIT_SHORTREV_HASH
+
+#ifdef GIT_SHORTREV_HASH
+	#define ALLTHEHAXX_VERSION "beta-13.1 (v0.9-3.1) #" GIT_SHORTREV_HASH
+#else
+	#define ALLTHEHAXX_VERSION "beta-13.1 (v0.9-3.1) custom"
+#endif
+
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 static const char GAME_ATH_VERSION[10] = "0.13.1"; // for updater
 
