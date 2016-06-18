dafb5d542e1877a5b6e6054ef6a7c347c25f7537
Henningstone <Henningstone@users.noreply.github.com>
2016-06-18 21:46:52 +0200

    emote menu can only be opened when online, fixes #159

--- src/game/client/components/emoticon.cpp
+++ src/game/client/components/emoticon.cpp
@@ -21,7 +21,7 @@ CEmoticon::CEmoticon()
 void CEmoticon::ConKeyEmoticon(IConsole::IResult *pResult, void *pUserData)
 {
 	CEmoticon *pSelf = (CEmoticon *)pUserData;
-	if(!pSelf->m_pClient->m_Snap.m_SpecInfo.m_Active && pSelf->Client()->State() != IClient::STATE_DEMOPLAYBACK)
+	if(!pSelf->m_pClient->m_Snap.m_SpecInfo.m_Active && pSelf->Client()->State() == IClient::STATE_ONLINE)
 		pSelf->m_Active = pResult->GetInteger(0) != 0;
 }
 
