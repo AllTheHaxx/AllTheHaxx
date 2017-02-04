370cbc551eaebb371244e4114c7099e462ee6c72
Henningstone <Henningstone@users.noreply.github.com>
2017-02-04 15:49:41 +0100

    0.28

--- src/game/version.h
+++ src/game/version.h
@@ -5,7 +5,7 @@
 #include "generated/nethash.cpp"
 
 #define GAME_VERSION "0.6.3"
-#define ATH_VERSION "0.27"
+#define ATH_VERSION "0.28"
 #define DDNET_VERSION "10.3.5"
 #define BUILD_DATE __DATE__ ", " __TIME__
 
@@ -19,7 +19,7 @@
 
 // for updater
 static const char GAME_ATH_VERSION[10] = ATH_VERSION;
-#define GAME_ATH_VERSION_NUMERIC 2700
+#define GAME_ATH_VERSION_NUMERIC 2800
 
 // do not modify these, they belong to ddnet
 static const char GAME_RELEASE_VERSION[8] = DDNET_VERSION;
