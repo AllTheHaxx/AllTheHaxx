d4b0dfe3a4a28113172af0cd89371937b523b464
Henningstone <Henningstone@users.noreply.github.com>
2017-03-27 00:43:49 +0200

    don't fetch skins with empty name

---
+++ src/engine/client/mapdownload.cpp
@@ -0,0 +1,5 @@
+//
+// Created by henningstone on 27.03.17.
+//
+
+#include "mapdownload.h"
---
+++ src/engine/client/mapdownload.h
@@ -0,0 +1,8 @@
+//
+// Created by henningstone on 27.03.17.
+//
+
+#ifndef ALLTHEHAXX_MAPDOWNLOAD_H
+#define ALLTHEHAXX_MAPDOWNLOAD_H
+
+#endif //ALLTHEHAXX_MAPDOWNLOAD_H
