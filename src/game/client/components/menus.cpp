AllTheHaxx/AllTheHaxx

commit 5bc2ec0b192df2ee40629533e46ae48e4192ee67

cleaned up Input()->KeyPress's in menus

src/game/client/components/menus.cpp
@@ -177,6 +177,27 @@ vec4 CMenus::ButtonColorMul(CButtonContainer *pBC)
 	return vec4(1,1,1,1);
 }
 
+bool CMenus::KeyEvent(int Key, int FlagMask)
+{
+	for(int i = 0; i < m_NumInputEvents; i++)
+		if(m_aInputEvents[i].m_Key == Key && (m_aInputEvents[i].m_Flags & FlagMask))
+			return true;
+	return false;
+}
+
+bool CMenus::KeyMods(int Keymod)
+{
+	switch(Keymod)
+	{
+		case KEYMOD_CTRL:
+			return Input()->KeyIsPressed(KEY_LCTRL) || Input()->KeyIsPressed(KEY_RCTRL);
+		case KEYMOD_SHIFT:
+			return Input()->KeyIsPressed(KEY_LSHIFT) || Input()->KeyIsPressed(KEY_RSHIFT);
+		default:
+			return false;
+	}
+}
+
 int CMenus::DoButton_Icon(int ImageId, int SpriteId, const CUIRect *pRect)
 {
 	CALLSTACK_ADD();
@@ -401,7 +422,7 @@ int CMenus::DoEditBox(CButtonContainer *pBC, const CUIRect *pRect, char *pStr, u
 
 			if(Event.m_Flags&IInput::FLAG_PRESS)
 			{
-				if(Input()->KeyIsPressed(KEY_LCTRL) || Input()->KeyIsPressed(KEY_RCTRL))
+				if(KeyMods(KEYMOD_CTRL))
 				{
 					Handled = true;
 
@@ -726,7 +747,7 @@ float CMenus::DoScrollbarV(CButtonContainer *pBC, const CUIRect *pRect, float Cu
 		if(!UI()->MouseButton(0))
 			UI()->SetActiveItem(0);
 
-		if(Input()->KeyIsPressed(KEY_LSHIFT) || Input()->KeyIsPressed(KEY_RSHIFT))
+		if(KeyMods(KEYMOD_SHIFT))
 			m_MouseSlow = true;
 
 		float Min = pRect->y;
@@ -772,13 +793,10 @@ float CMenus::DoScrollbarV(CButtonContainer *pBC, const CUIRect *pRect, float Cu
 
 	if(UI()->MouseInside(&Rail) || Inside)
 	{
-		if(m_pClient->m_pGameConsole->IsClosed())
-		{
-			if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP))
-				ReturnValue -= Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
-			else if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN))
-				ReturnValue += Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
-		}
+		if(KeyEvent(KEY_MOUSE_WHEEL_UP))
+			ReturnValue -= Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
+		else if(KeyEvent(KEY_MOUSE_WHEEL_DOWN))
+			ReturnValue += Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
 	}
 
 	return clamp(ReturnValue, 0.0f, 1.0f);
@@ -821,7 +839,7 @@ float CMenus::DoScrollbarH(CButtonContainer *pBC, const CUIRect *pRect, float Cu
 		if(!UI()->MouseButton(0))
 			UI()->SetActiveItem(0);
 
-		if(Input()->KeyIsPressed(KEY_LSHIFT) || Input()->KeyIsPressed(KEY_RSHIFT))
+		if(KeyMods(KEYMOD_SHIFT))
 			m_MouseSlow = true;
 
 		float Min = pRect->x;
@@ -890,13 +908,10 @@ float CMenus::DoScrollbarH(CButtonContainer *pBC, const CUIRect *pRect, float Cu
 
 	if(UI()->MouseInside(&Rail) || Inside)
 	{
-		if(m_pClient->m_pGameConsole->IsClosed())
-		{
-			if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP))
-				ReturnValue += Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
-			else if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN))
-				ReturnValue -= Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
-		}
+		if(KeyEvent(KEY_MOUSE_WHEEL_UP))
+			ReturnValue += Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
+		else if(KeyEvent(KEY_MOUSE_WHEEL_DOWN))
+			ReturnValue -= Input()->KeyPress(KEY_LSHIFT) ? 0.01f : 0.05f;
 	}
 
 	return clamp(ReturnValue, 0.0f, 1.0f);
@@ -1554,9 +1569,9 @@ void CMenus::RenderNews(CUIRect MainView)
 		// scroll with the mousewheel
 		if(m_pClient->m_pGameConsole->IsClosed())
 		{
-			if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN))
+			if(KeyEvent(KEY_MOUSE_WHEEL_DOWN))
 				s_WantedScrollOffset[CURRENT_NEWS_PAGE] += 0.1f;
-			if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP))
+			else if(KeyEvent(KEY_MOUSE_WHEEL_UP))
 				s_WantedScrollOffset[CURRENT_NEWS_PAGE] -= 0.1f;
 		}
 		s_WantedScrollOffset[CURRENT_NEWS_PAGE] = clamp(s_WantedScrollOffset[CURRENT_NEWS_PAGE], 0.0f, 1.0f);
@@ -2630,9 +2645,12 @@ void CMenus::OnRender()
 	{
 		if(m_UseMouseButtons)
 		{
-			if(Input()->KeyIsPressed(KEY_MOUSE_1)) Buttons |= 1;
-			if(Input()->KeyIsPressed(KEY_MOUSE_2)) Buttons |= 2;
-			if(Input()->KeyIsPressed(KEY_MOUSE_3)) Buttons |= 4;
+			for(int i = 0; i < m_NumInputEvents; i++)
+			{
+				if(m_aInputEvents[i].m_Key == KEY_MOUSE_1) Buttons |= 1;
+				if(m_aInputEvents[i].m_Key == KEY_MOUSE_2) Buttons |= 2;
+				if(m_aInputEvents[i].m_Key == KEY_MOUSE_3) Buttons |= 4;
+			}
 		}
 #if defined(__ANDROID__)
 		static int ButtonsOneFrameDelay = 0; // For Android touch input
@@ -2706,40 +2724,40 @@ void CMenus::RenderBackground()
 	// render background color
 	Graphics()->TextureSet(-1);
 	Graphics()->QuadsBegin();
-		//vec4 bottom(gui_color.r*0.3f, gui_color.g*0.3f, gui_color.b*0.3f, 1.0f);
-		//vec4 bottom(0, 0, 0, 1.0f);
-		vec4 Bottom(ms_GuiColor.r, ms_GuiColor.g, ms_GuiColor.b, 1.0f);
-		vec4 Top(ms_GuiColor.r, ms_GuiColor.g, ms_GuiColor.b, 1.0f);
-		IGraphics::CColorVertex Array[4] = {
+	//vec4 bottom(gui_color.r*0.3f, gui_color.g*0.3f, gui_color.b*0.3f, 1.0f);
+	//vec4 bottom(0, 0, 0, 1.0f);
+	vec4 Bottom(ms_GuiColor.r, ms_GuiColor.g, ms_GuiColor.b, 1.0f);
+	vec4 Top(ms_GuiColor.r, ms_GuiColor.g, ms_GuiColor.b, 1.0f);
+	IGraphics::CColorVertex Array[4] = {
 			IGraphics::CColorVertex(0, Top.r, Top.g, Top.b, Top.a),
 			IGraphics::CColorVertex(1, Top.r, Top.g, Top.b, Top.a),
 			IGraphics::CColorVertex(2, Bottom.r, Bottom.g, Bottom.b, Bottom.a),
 			IGraphics::CColorVertex(3, Bottom.r, Bottom.g, Bottom.b, Bottom.a)};
-		Graphics()->SetColorVertex(Array, 4);
-		IGraphics::CQuadItem QuadItem(0, 0, sw, sh);
-		Graphics()->QuadsDrawTL(&QuadItem, 1);
+	Graphics()->SetColorVertex(Array, 4);
+	IGraphics::CQuadItem QuadItem(0, 0, sw, sh);
+	Graphics()->QuadsDrawTL(&QuadItem, 1);
 	Graphics()->QuadsEnd();
 
 	// render the tiles
 	Graphics()->TextureSet(-1);
 	Graphics()->QuadsBegin();
-		float Size = 15.0f;
-		float OffsetTime = (float)fmod(Client()->LocalTime() * 0.15f, 2.0f);
-		for(int y = -2; y < (int)(sw/Size); y++)
-			for(int x = -2; x < (int)(sh/Size); x++)
-			{
-				Graphics()->SetColor(0,0,0,0.045f);
-				IGraphics::CQuadItem QuadItem((x-OffsetTime)*Size*2+(y&1)*Size, (y+OffsetTime)*Size, Size, Size);
-				Graphics()->QuadsDrawTL(&QuadItem, 1);
-			}
+	float Size = 15.0f;
+	float OffsetTime = (float)fmod(Client()->LocalTime() * 0.15f, 2.0f);
+	for(int y = -2; y < (int)(sw/Size); y++)
+		for(int x = -2; x < (int)(sh/Size); x++)
+		{
+			Graphics()->SetColor(0,0,0,0.045f);
+			IGraphics::CQuadItem QuadItem((x-OffsetTime)*Size*2+(y&1)*Size, (y+OffsetTime)*Size, Size, Size);
+			Graphics()->QuadsDrawTL(&QuadItem, 1);
+		}
 	Graphics()->QuadsEnd();
 
 	// render border fade
 	Graphics()->TextureSet(gs_TextureBlob);
 	Graphics()->QuadsBegin();
-		Graphics()->SetColor(1,1,1,1);
-		QuadItem = IGraphics::CQuadItem(-100, -100, sw+200, sh+200);
-		Graphics()->QuadsDrawTL(&QuadItem, 1);
+	Graphics()->SetColor(1,1,1,1);
+	QuadItem = IGraphics::CQuadItem(-100, -100, sw+200, sh+200);
+	Graphics()->QuadsDrawTL(&QuadItem, 1);
 	Graphics()->QuadsEnd();
 
 	// EVENT CALL
@@ -2747,7 +2765,7 @@ void CMenus::RenderBackground()
 
 	// restore screen
 	{CUIRect Screen = *UI()->Screen();
-	Graphics()->MapScreen(Screen.x, Screen.y, Screen.w, Screen.h);}
+		Graphics()->MapScreen(Screen.x, Screen.y, Screen.w, Screen.h);}
 }
 
 int CMenus::DoButton_CheckBox_DontCare(CButtonContainer *pBC, const char *pText, int Checked, const CUIRect *pRect)
@@ -2756,14 +2774,14 @@ int CMenus::DoButton_CheckBox_DontCare(CButtonContainer *pBC, const char *pText,
 
 	switch(Checked)
 	{
-	case 0:
-		return DoButton_CheckBox_Common(pBC, pText, "", pRect);
-	case 1:
-		return DoButton_CheckBox_Common(pBC, pText, "X", pRect);
-	case 2:
-		return DoButton_CheckBox_Common(pBC, pText, "O", pRect);
-	default:
-		return DoButton_CheckBox_Common(pBC, pText, "", pRect);
+		case 0:
+			return DoButton_CheckBox_Common(pBC, pText, "", pRect);
+		case 1:
+			return DoButton_CheckBox_Common(pBC, pText, "X", pRect);
+		case 2:
+			return DoButton_CheckBox_Common(pBC, pText, "O", pRect);
+		default:
+			return DoButton_CheckBox_Common(pBC, pText, "", pRect);
 	}
 }
 

src/game/client/components/menus.h
@@ -95,6 +95,13 @@ private:
 
 	vec4 ButtonColorMul(CButtonContainer *pBC);
 
+	enum
+	{
+		KEYMOD_CTRL,
+		KEYMOD_SHIFT
+	};
+	bool KeyEvent(int Key, int FlagMask = IInput::FLAG_PRESS);
+	bool KeyMods(int Keymod);
 
 	int DoButton_DemoPlayer(CButtonContainer *pBC, const char *pText, int Checked, const CUIRect *pRect);
 

src/game/client/components/menus_browser.cpp
@@ -182,20 +182,20 @@ void CMenus::RenderServerbrowserServerList(CUIRect View)
 			s_WantedScrollValue = (float)(m_ScrollOffset)/ScrollNum;
 			m_ScrollOffset = -1;
 		}
-		if(UI()->MouseInside(&View) && m_pClient->m_pGameConsole->IsClosed())
+		if(UI()->MouseInside(&View))
 		{
-			if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP))
+			if(KeyEvent(KEY_MOUSE_WHEEL_UP))
 				s_WantedScrollValue -= Input()->KeyIsPressed(KEY_LSHIFT) ? 1.5f / ScrollNum : Input()->KeyIsPressed(KEY_LCTRL) ? 6.0f / ScrollNum : 3.0f / ScrollNum;
-			if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN))
+			else if(KeyEvent(KEY_MOUSE_WHEEL_DOWN))
 				s_WantedScrollValue += Input()->KeyIsPressed(KEY_LSHIFT) ? 1.5f / ScrollNum : Input()->KeyIsPressed(KEY_LCTRL) ? 6.0f / ScrollNum : 3.0f / ScrollNum;
 		}
 	}
 	else
 		ScrollNum = 0;
 
-	if(Input()->KeyPress(KEY_TAB) && m_pClient->m_pGameConsole->IsClosed())
+	if(KeyEvent(KEY_TAB))
 	{
-		if(Input()->KeyIsPressed(KEY_LSHIFT) || Input()->KeyIsPressed(KEY_RSHIFT))
+		if(KeyMods(KEYMOD_SHIFT))
 			g_Config.m_UiToolboxPage = (g_Config.m_UiToolboxPage + 3 - 1) % 3;
 		else
 			g_Config.m_UiToolboxPage = (g_Config.m_UiToolboxPage + 3 + 1) % 3;
@@ -1517,7 +1517,7 @@ void CMenus::RenderServerbrowser(CUIRect MainView)
 			str_copy(aBuf, Localize("Refresh"), sizeof(aBuf));
 
 		static CButtonContainer s_RefreshButton;
-		if(DoButton_Menu(&s_RefreshButton, aBuf, 0, &Button, Localize("Refresh the serverlist completely"), ServerBrowser()->IsRefreshing() ? CUI::CORNER_L : CUI::CORNER_ALL) || Input()->KeyPress(KEY_F5) || Input()->KeyPress(KEY_F5) || (Input()->KeyPress(KEY_R) && (Input()->KeyIsPressed(KEY_LCTRL) || Input()->KeyIsPressed(KEY_RCTRL))))
+		if(DoButton_Menu(&s_RefreshButton, aBuf, 0, &Button, Localize("Refresh the serverlist completely"), ServerBrowser()->IsRefreshing() ? CUI::CORNER_L : CUI::CORNER_ALL) || (KeyMods(KEYMOD_CTRL) && KeyEvent(KEY_R)))
 		{
 			if(g_Config.m_UiBrowserPage == PAGE_BROWSER_INTERNET)
 				ServerBrowser()->Refresh(IServerBrowser::TYPE_INTERNET);

src/game/client/components/menus_demo.cpp
@@ -286,7 +286,7 @@ void CMenus::RenderDemoPlayer(CUIRect MainView)
 				static float PrevAmount = 0.0f;
 				float Amount = (UI()->MouseX()-SeekBar.x)/SeekBar.w;
 
-				if(Input()->KeyIsPressed(KEY_LSHIFT) || Input()->KeyIsPressed(KEY_RSHIFT))
+				if(KeyMods(KEYMOD_SHIFT))
 				{
 					Amount = PrevAmount + (Amount-PrevAmount) * 0.05f;
 
@@ -752,9 +752,9 @@ void CMenus::RenderDemoList(CUIRect MainView)
 			s_ScrollValue = (float)(m_ScrollOffset)/ScrollNum;
 			m_ScrollOffset = 0;
 		}
-		if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP) && UI()->MouseInside(&ListBox) && m_pClient->m_pGameConsole->IsClosed())
+		if(KeyEvent(KEY_MOUSE_WHEEL_UP) && UI()->MouseInside(&ListBox))
 			s_ScrollValue -= 3.0f/ScrollNum;
-		if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN) && UI()->MouseInside(&ListBox) && m_pClient->m_pGameConsole->IsClosed())
+		else if(KeyEvent(KEY_MOUSE_WHEEL_DOWN) && UI()->MouseInside(&ListBox))
 			s_ScrollValue += 3.0f/ScrollNum;
 	}
 	else

src/game/client/components/menus_ingame.cpp
@@ -369,7 +369,7 @@ void CMenus::RenderServerConfigCreator(CUIRect MainView)
 	Button.VSplitMid(&Button, &Button2);
 	Button.VSplitRight(10.0f, &Button, 0); Button2.VSplitLeft(10.0f, 0, &Button2);
 	static CButtonContainer s_AddEntryButton;
-	if(DoButton_Menu(&s_AddEntryButton, Localize("Add"), 0, &Button) || (UI()->MouseInside(&MainView) && Input()->KeyPress(KEY_RETURN)))
+	if(DoButton_Menu(&s_AddEntryButton, Localize("Add"), 0, &Button) || (UI()->MouseInside(&MainView) && KeyEvent(KEY_RETURN)))
 	{
 		ConfigCreatorEntry e;
 		s_Items.add(e);
@@ -2036,11 +2036,11 @@ void CMenus::RenderGhost(CUIRect MainView)
 	s_ScrollValue = DoScrollbarV(&s_ScrollBar, &Scroll, s_ScrollValue);
 
 	int ScrollNum = NumGhosts-Num+1;
-	if(ScrollNum > 0 && m_pClient->m_pGameConsole->IsClosed())
+	if(ScrollNum > 0)
 	{
-		if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP))
+		if(KeyEvent(KEY_MOUSE_WHEEL_UP))
 			s_ScrollValue -= 1.0f/ScrollNum;
-		if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN))
+		else if(KeyEvent(KEY_MOUSE_WHEEL_DOWN))
 			s_ScrollValue += 1.0f/ScrollNum;
 	}
 	else

src/game/client/components/menus_irc.cpp
@@ -91,11 +91,6 @@ void CMenus::RenderIRC(CUIRect MainView)
 	CUIRect MainIRC, EntryBox, Button;
 	MainView.Margin(10.0f, &MainIRC);
 
-	/*if (m_GamePagePanel != PANEL_CHAT && UI()->MouseInside(&MainView) && Input()->KeyPressed(KEY_MOUSE_1))
-	 {
-	 m_GamePagePanel = PANEL_CHAT;
-	 }*/
-
 	if(m_pClient->IRC()->GetState() == IIRC::STATE_DISCONNECTED)
 	{
 		EntryBox.x = MainIRC.x + (MainIRC.w / 2.0f - 300.0f / 2.0f);

src/game/client/components/menus_manual.cpp
@@ -88,13 +88,10 @@ void CMenus::RenderManual(CUIRect MainView)
 			static CButtonContainer s_Scrollbar; \
 			s_WantedScrollOffset = DoScrollbarV(&s_Scrollbar, &Scrollbar, s_WantedScrollOffset); \
 			\
-			if(m_pClient->m_pGameConsole->IsClosed()) \
-			{ \
-				if(Input()->KeyPress(KEY_MOUSE_WHEEL_DOWN)) \
-					s_WantedScrollOffset += 0.1f; \
-				if(Input()->KeyPress(KEY_MOUSE_WHEEL_UP)) \
-					s_WantedScrollOffset -= 0.1f; \
-			} \
+			if(KeyEvent(KEY_MOUSE_WHEEL_DOWN)) \
+				s_WantedScrollOffset += 0.1f; \
+			else if(KeyEvent(KEY_MOUSE_WHEEL_UP)) \
+				s_WantedScrollOffset -= 0.1f; \
 			s_WantedScrollOffset = clamp(s_WantedScrollOffset, 0.0f, 1.0f); \
 			\
 			smooth_set(&s_ScrollOffset, s_WantedScrollOffset, 27.0f, Client()->RenderFrameTime()); \

src/game/client/components/menus_settings/settings_lua.cpp
@@ -235,7 +235,7 @@ void CMenus::RenderSettingsLua(CUIRect MainView)
 
 				if(Item.m_Rect.y+Item.m_Rect.h > Item.m_HitRect.y)
 				{
-					if(UI()->MouseInside(&Item.m_Rect) && Input()->KeyPress(KEY_MOUSE_1))
+					if(UI()->MouseInside(&Item.m_Rect) && KeyEvent(KEY_MOUSE_1))
 						s_SelectedScript = i;
 
 					// activate button

commit ca4bb1a3b3c9e324772327b9b4f3548fcbf7d4cf

Fixed mousebutton handling in menus
was broken by 5bc2ec0b192df2ee40629533e46ae48e4192ee67 / 2f839917546f0aa9e53e7fe40c45f904098b3421

src/game/client/components/menus.cpp
@@ -2644,15 +2644,16 @@ void CMenus::OnRender()
 	{
 		if(m_UseMouseButtons)
 		{
-			for(int i = 0; i < m_NumInputEvents; i++)
-			{
-				if(m_aInputEvents[i].m_Flags == IInput::FLAG_PRESS)
-				{
-					if(m_aInputEvents[i].m_Key == KEY_MOUSE_1) Buttons |= 1;
-					if(m_aInputEvents[i].m_Key == KEY_MOUSE_2) Buttons |= 2;
-					if(m_aInputEvents[i].m_Key == KEY_MOUSE_3) Buttons |= 4;
-				}
-			}
+			if(Input()->KeyIsPressed(KEY_MOUSE_1)) Buttons |= 1;
+			if(Input()->KeyIsPressed(KEY_MOUSE_2)) Buttons |= 2;
+			if(Input()->KeyIsPressed(KEY_MOUSE_3)) Buttons |= 4;
+
+		/*	dbg_msg("dbg", "mouse buttons: %s%s%s",
+					Buttons&1 ? "x" : ".",
+					Buttons&4 ? "x" : ".",
+					Buttons&2 ? "x" : "."
+			);	*/
+
 		}
 #if defined(__ANDROID__)
 		static int ButtonsOneFrameDelay = 0; // For Android touch input