912bac338838bace123fcf06c151545c0f21f1bd
Henningstone <Henningstone@users.noreply.github.com>
2017-04-10 15:39:27 +0200

    LuaLib: added appropriate error messages to containers.lua

--- data/lua/.library/containers.lua
+++ data/lua/.library/containers.lua
@@ -16,6 +16,7 @@ end
 
 
 function NextBC()
+    if _ButtonContainers[ActiveGroup] == nil or iCurrBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use NextBC()", 2) end
     iCurrBC[ActiveGroup] = iCurrBC[ActiveGroup] + 1
     if _ButtonContainers[ActiveGroup][iCurrBC[ActiveGroup]] == nil then
         _ButtonContainers[ActiveGroup][iCurrBC[ActiveGroup]] = ButtonContainer()
@@ -24,15 +25,18 @@ function NextBC()
 end
 
 function CurrBC()
+    if _ButtonContainers[ActiveGroup] == nil or iCurrBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use CurrBC()", 2) end
     return _ButtonContainers[ActiveGroup][iCurrBC[ActiveGroup]]
 end
 
 function SetBC(bc, i)
+    if _ButtonContainers[ActiveGroup] == nil or iCurrBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use SetBC()", 2) end
     i = i or iCurrBC[ActiveGroup]
     _ButtonContainers[ActiveGroup][i] = bc
 end
 
 function GetBC(i)
+    if _ButtonContainers[ActiveGroup] == nil or iCurrBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use GetBC()", 2) end
     if i == nil or i == 0 then i = iCurrBC[ActiveGroup] end
     if i < 0 then i = iCurrBC[ActiveGroup] + i end
     return _ButtonContainers[ActiveGroup][i]
@@ -40,6 +44,7 @@ end
 
 
 function NextEBC()
+    if _EditboxContainers[ActiveGroup] == nil or iCurrEBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use NextEBC()", 2) end
     iCurrEBC[ActiveGroup] = iCurrEBC[ActiveGroup] + 1
     if _EditboxContainers[ActiveGroup][iCurrEBC[ActiveGroup]] == nil then
         _EditboxContainers[ActiveGroup][iCurrEBC[ActiveGroup]] = EditboxContainer()
@@ -48,15 +53,18 @@ function NextEBC()
 end
 
 function CurrEBC()
+    if _EditboxContainers[ActiveGroup] == nil or iCurrEBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use CurrEBC()", 2) end
     return _EditboxContainers[ActiveGroup][iCurrEBC[ActiveGroup]]
 end
 
 function SetEBC(ebc, i)
+    if _EditboxContainers[ActiveGroup] == nil or iCurrEBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use SetEBC()", 2) end
     if i == nil then i = iCurrEBC[ActiveGroup] end
     _EditboxContainers[ActiveGroup][iCurrEBC[ActiveGroup]] = ebc
 end
 
 function GetEBC(i)
+    if _EditboxContainers[ActiveGroup] == nil or iCurrEBC[ActiveGroup] == nil then error("You must use ContainersStart() before you can use GetEBC()", 2) end
     if i == nil or i == 0 then i = iCurrEBC[ActiveGroup] end
     if i < 0 then i = iCurrEBC[ActiveGroup] + i end
     return _EditboxContainers[ActiveGroup][i]
