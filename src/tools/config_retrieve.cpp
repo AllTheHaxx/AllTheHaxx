TeeworldsCN/ddnet-pvp

Add map_diff tool to compare tile layers of map versions

src/tools/config_retrieve.cpp
@@ -6,7 +6,7 @@
 void Process(IStorage *pStorage, const char *pMapName, const char *pConfigName)
 {
 	CDataFileReader Map;
-	if(!Map.Open(pStorage, pMapName, IStorage::TYPE_ALL))
+	if(!Map.Open(pStorage, pMapName, IStorage::TYPE_ABSOLUTE))
 	{
 		dbg_msg("config_retrieve", "error opening map '%s'", pMapName);
 		return;
@@ -28,7 +28,7 @@ void Process(IStorage *pStorage, const char *pMapName, const char *pConfigName)
 			break;
 
 		ConfigFound = true;
-		IOHANDLE Config = pStorage->OpenFile(pConfigName, IOFLAG_WRITE, IStorage::TYPE_ALL);
+		IOHANDLE Config = pStorage->OpenFile(pConfigName, IOFLAG_WRITE, IStorage::TYPE_ABSOLUTE);
 		if(!Config)
 		{
 			dbg_msg("config_retrieve", "error opening config for writing '%s'", pConfigName);

src/tools/config_store.cpp
@@ -7,7 +7,7 @@
 
 void Process(IStorage *pStorage, const char *pMapName, const char *pConfigName)
 {
-	IOHANDLE File = pStorage->OpenFile(pConfigName, IOFLAG_READ, IStorage::TYPE_ALL);
+	IOHANDLE File = pStorage->OpenFile(pConfigName, IOFLAG_READ, IStorage::TYPE_ABSOLUTE);
 	array<char *> aLines;
 	char *pSettings = NULL;
 	if(!File)
@@ -42,7 +42,7 @@ void Process(IStorage *pStorage, const char *pMapName, const char *pConfigName)
 	}
 
 	CDataFileReader Reader;
-	Reader.Open(pStorage, pMapName, IStorage::TYPE_ALL);
+	Reader.Open(pStorage, pMapName, IStorage::TYPE_ABSOLUTE);
 
 	CDataFileWriter Writer;
 	Writer.Init();

src/tools/map_resave.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char **argv)
 
 	str_format(aFileName, sizeof(aFileName), "%s", argv[2]);
 
-	if(!DataFile.Open(pStorage, argv[1], IStorage::TYPE_ALL))
+	if(!DataFile.Open(pStorage, argv[1], IStorage::TYPE_ABSOLUTE))
 		return -1;
 	if(!df.Open(pStorage, aFileName))
 		return -1;

AllTheHaxx/AllTheHaxx

(cherry picked from commit f2444b0)

src/tools/config_retrieve.cpp
@@ -6,7 +6,7 @@
 void Process(IStorageTW *pStorage, const char *pMapName, const char *pConfigName)
 {
 	CDataFileReader Map;
-	if(!Map.Open(pStorage, pMapName, IStorageTW::TYPE_ALL))
+	if(!Map.Open(pStorage, pMapName, IStorageTW::TYPE_ABSOLUTE))
 	{
 		dbg_msg("config_retrieve", "error opening map '%s'", pMapName);
 		return;
@@ -28,7 +28,7 @@ void Process(IStorageTW *pStorage, const char *pMapName, const char *pConfigName
 			break;
 
 		ConfigFound = true;
-		IOHANDLE Config = pStorage->OpenFile(pConfigName, IOFLAG_WRITE, IStorageTW::TYPE_ALL);
+		IOHANDLE Config = pStorage->OpenFile(pConfigName, IOFLAG_WRITE, IStorageTW::TYPE_ABSOLUTE);
 		if(!Config)
 		{
 			dbg_msg("config_retrieve", "error opening config for writing '%s'", pConfigName);

src/tools/config_store.cpp
@@ -7,7 +7,7 @@
 
 void Process(IStorageTW *pStorage, const char *pMapName, const char *pConfigName)
 {
-	IOHANDLE File = pStorage->OpenFile(pConfigName, IOFLAG_READ, IStorageTW::TYPE_ALL);
+	IOHANDLE File = pStorage->OpenFile(pConfigName, IOFLAG_READ, IStorageTW::TYPE_ABSOLUTE);
 	array<char *> aLines;
 	char *pSettings = NULL;
 	if(!File)
@@ -42,7 +42,7 @@ void Process(IStorageTW *pStorage, const char *pMapName, const char *pConfigName
 	}
 
 	CDataFileReader Reader;
-	Reader.Open(pStorage, pMapName, IStorageTW::TYPE_ALL);
+	Reader.Open(pStorage, pMapName, IStorageTW::TYPE_ABSOLUTE);
 
 	CDataFileWriter Writer;
 	Writer.Init();

src/tools/map_resave.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char **argv)
 
 	str_format(aFileName, sizeof(aFileName), "%s", argv[2]);
 
-	if(!DataFile.Open(pStorage, argv[1], IStorageTW::TYPE_ALL))
+	if(!DataFile.Open(pStorage, argv[1], IStorageTW::TYPE_ABSOLUTE))
 		return -1;
 	if(!df.Open(pStorage, aFileName))
 		return -1;