fb2dc6cc857cf055187f752a75bd951f34da2820
Henningstone <Henningstone@users.noreply.github.com>
2017-09-21 19:46:45 +0200

    Explicitly enable C++11
    Made warning about lambda usage disappear

--- bam.lua
+++ bam.lua
@@ -267,6 +267,7 @@ function build(settings)
 	else
 		settings.cc.flags:Add("-Wall")
 		settings.cc.flags:Add("-Wno-deprecated", "-Werror=format")
+		settings.cc.flags_cxx:Add("-std=c++11")
 		if family == "windows" then
 			if config.compiler.driver == "gcc" then
 				settings.link.flags:Add("-static-libgcc")
