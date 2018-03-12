939294d00d0388f061c1c5e3f66a61dc928addaa
Henningstone <Henningstone@users.noreply.github.com>
2018-03-12 21:55:42 +0100

    Only use night mode if menu background is enabled

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -2213,7 +2213,7 @@ void CMenus::OnRender()
 		}
 	}
 
-	if(s_IsNightTime == -1)
+	if(s_IsNightTime == -1 || !g_Config.m_ClMenuBackground)
 	{
 		ms_ColorTabbarInactiveOutgame = vec4(0, 0, 0, 0.25f);
 		ms_ColorTabbarActiveOutgame = vec4(0, 0, 0, 0.5f);
