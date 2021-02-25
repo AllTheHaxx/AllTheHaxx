c9671d8d951360aa0bbc4560b97380948d855b0e
GutZuFusss <leon.ikinger@googlemail.com>
2021-02-25 07:40:25 +0100

    enable br_show_ddnet by default

--- src/game/variables.h
+++ src/game/variables.h
@@ -163,7 +163,7 @@ MACRO_CONFIG_INT(ClOutgoingPort, cl_outgoing_port, 0, 0, 65535, CFGFLAG_CLIENT, 
 // haxx
 MACRO_CONFIG_INT(BrAutoRefresh, br_auto_refresh, 60, 0, 3600, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Time in secs to refresh the serverbrowser when active (0 disables)")
 MACRO_CONFIG_INT(BrAutoCache, br_auto_cache, 1, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Enable automatic serverlist cache management (recommended)")
-MACRO_CONFIG_INT(BrShowDDNet, br_show_ddnet, 0, 0, 2, CFGFLAG_CLIENT|CFGFLAG_SAVE, "0: Show in serverlist (hide Tab) | 1: Move servers to DDNet Tab | 2: Show in both")
+MACRO_CONFIG_INT(BrShowDDNet, br_show_ddnet, 1, 0, 2, CFGFLAG_CLIENT|CFGFLAG_SAVE, "0: Show in serverlist (hide Tab) | 1: Move servers to DDNet Tab | 2: Show in both")
 MACRO_CONFIG_INT(BrAllowPureMod, br_allow_pure_mod, 0, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Allow joining modded pure server")
 MACRO_CONFIG_INT(BrIgnoreConnecting, br_ignore_connecting, 1, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Ignore (connecting) clients on servers")
 //MACRO_CONFIG_INT(BrLazySorting, br_lazy_sorting, 1, 0, 1, CFGFLAG_CLIENT|CFGFLAG_SAVE, "Only sort the serverlist once when reloading has finished")
